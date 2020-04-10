6ee38a41506cdf8541dfec26e00dcd8f96c6c93b
WuJang <2312389566@qq.com>
2020-04-10 17:06:24 +0800

    第二次提交

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <div class="mdui-fab-wrapper" id="exampleFab">
+      <button class="mdui-fab mdui-ripple mdui-color-theme-accent">
+        <!-- 默认显示的图标 -->
+        <i class="mdui-icon material-icons">add</i>
+
+        <!-- 在拨号菜单开始打开时，平滑切换到该图标，若不需要切换图标，则可以省略该元素 -->
+        <i class="mdui-icon mdui-fab-opened material-icons">add</i>
+      </button>
+      <div class="mdui-fab-dial">
+        <button class="mdui-fab mdui-fab-mini mdui-ripple mdui-color-pink"><i class="mdui-icon material-icons">backup</i></button>
+        <button class="mdui-fab mdui-fab-mini mdui-ripple mdui-color-red"><i class="mdui-icon material-icons">bookmark</i></button>
+        <button class="mdui-fab mdui-fab-mini mdui-ripple mdui-color-orange"><i class="mdui-icon material-icons">access_alarms</i></button>
+        <button class="mdui-fab mdui-fab-mini mdui-ripple mdui-color-blue"><i class="mdui-icon material-icons">touch_app</i></button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      progress: false
+    }
+  },
+  methods:{
+    // 刷新
+    refresh(){
+      this.progress = true
+      setTimeout(()=>{
+        this.progress = false
+      },1000)
+      // location.reload()
+    },
+  }
+}
+</script>
+
+<style>
+  html{
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="mdui-progress" v-if="progress">
+      <div class="mdui-progress-indeterminate" style="background: #ce156c"></div>
+    </div>
+    <div class="mdui-toolbar mdui-color-theme" style="background: cadetblue;">
+      <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">menu</i></a>
+      <span class="mdui-typo-title">Wuboing</span>
+      <div class="mdui-toolbar-spacer"></div>
+      <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">search</i></a>
+      <a href="javascript:;" @click="refresh" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>
+      <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">more_vert</i></a>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "index",
+      data(){
+        return{
+          progress: false
+        }
+      },
+      methods:{
+        // 刷新
+        refresh(){
+          this.progress = true
+          setTimeout(()=>{
+            this.progress = false
+          },1000)
+          // location.reload()
+        },
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
