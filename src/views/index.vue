3c3ddb891292561e5f89800413791e82233dff0e
WuJang <2312389566@qq.com>
2020-04-28 09:24:08 +0800

    侧边菜单（待优化）

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,12 +4,45 @@
       <div class="mdui-progress-indeterminate" style="background: #ce156c"></div>
     </div>
     <div class="mdui-toolbar mdui-color-theme" style="background: cadetblue;">
-      <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">menu</i></a>
+      <a href="javascript:;"  id="toggle" @click="abc" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">menu</i></a>
       <span class="mdui-typo-title">Wuboing</span>
       <div class="mdui-toolbar-spacer"></div>
       <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">search</i></a>
       <a href="javascript:;" @click="refresh" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>
       <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">more_vert</i></a>
+    </div>
+    <div class="mdui-drawer" id="drawer" style="margin-top: 56px">
+      <ul class="mdui-list">
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">move_to_inbox</i>
+          <div class="mdui-list-item-content">Inbox</div>
+        </li>
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">star</i>
+          <div class="mdui-list-item-content">Starred</div>
+        </li>
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+          <div class="mdui-list-item-content">Sent mail</div>
+        </li>
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">drafts</i>
+          <div class="mdui-list-item-content">Drafts</div>
+        </li>
+        <li class="mdui-subheader">Subheader</li>
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">email</i>
+          <div class="mdui-list-item-content">All mail</div>
+        </li>
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">delete</i>
+          <div class="mdui-list-item-content">Trash</div>
+        </li>
+        <li class="mdui-list-item mdui-ripple">
+          <i class="mdui-list-item-icon mdui-icon material-icons">error</i>
+          <div class="mdui-list-item-content">Spam</div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -30,6 +63,12 @@
             this.progress = false
           },1000)
           // location.reload()
+        },
+        abc(){
+          var inst = new mdui.Drawer('#drawer');
+          document.getElementById('toggle').addEventListener('click', function () {
+            inst.toggle();
+          });
         },
       }
     }
